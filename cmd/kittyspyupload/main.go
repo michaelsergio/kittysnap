60fefb7956c22264d9eca7e64797860dd61a0758
Mike Sergio <mike.sergio@docurated.com>
2016-04-12 22:59:53 -0400

    Added main file.

--- cmd/kittyspyupload/main.go
+++ cmd/kittyspyupload/main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"github.com/michaelsergio/kittyspy"
+	//"path/filepath"
+	"fmt"
+	//"time"
+
+	. "github.com/michaelsergio/kittyspy"
 )
 
+type Context struct {
+	uploader Uploader
+	camera   Camera
+}
+
+func RunEverySeconds(seconds uint32, f func()) {
+	// Write image to tmp storage
+	//timer := time.NewTimer(time.Duration(seconds) * time.Second)
+	// TODO: Timer currently does not tick more than once.
+	f()
+
+}
+
 func main() {
-	kittyspy.Upload()
+	camera := NewMockCamera("test.jpg")
+	uploader := NewMockUploader(0)
+
+	ctx := Context{uploader: &uploader, camera: &camera}
+
+	RunEverySeconds(10, func() {
+		imagefile := ctx.camera.TakeImage()
+		result, err := ctx.uploader.Upload(imagefile)
+		if err != nil {
+			fmt.Println("failed to upload:", err)
+			return
+		}
+		fmt.Println("Uploaded:", result)
+	})
 }
