30b51b094431cd4e53c76fef49504297bb6dbd28
Mike Sergio <mikeserg@gmail.com>
2016-04-29 09:36:51 -0400

    Removed unused code in uploader.

--- upload.go
+++ upload.go
@@ -1,13 +1,10 @@
 package kittysnap
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"strings" // replace with io reader later.
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
@@ -33,15 +30,13 @@ func NewS3Uploader(conf *Conf) S3Uploader {
 	return uploader
 }
 
-func (up *S3Uploader) Upload(key string, filename string) (UploaderResult, error) {
-	// TODO: define Bucket name
-	// Replace Body with file IO reader.
-	// TODO define keyname
-
+func (up *S3Uploader) Upload(key, filename string) (UploaderResult, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return "", err
 	}
+
+	log.Println("Uploading", filename, "to s3 with key", key)
 
 	uploadResult, err := up.svc.PutObject(&s3.PutObjectInput{
 		Body:   file,
@@ -54,69 +49,4 @@ func (up *S3Uploader) Upload(key string, filename string) (UploaderResult, error
 
 	ures := UploaderResult(uploadResult.String())
 	return ures, nil
-}
-
-func Upload() {
-	fmt.Println("Uploading....")
-
-	//svc := s3.New(session.New(), &aws.Config{Region: aws.String("mock-region")})
-	creds := credentials.NewEnvCredentials()
-	config := aws.NewConfig().
-		WithRegion("us-east-1").
-		WithCredentials(creds).
-		WithCredentialsChainVerboseErrors(true)
-	// config := &aws.Config{Region: aws.String("us-west-2")}
-	// config = config.WithCredentialsChainVerboseErrors(true)
-	svc := s3.New(session.New(), config)
-
-	// List buckets
-	result, err := svc.ListBuckets(&s3.ListBucketsInput{})
-	if err != nil {
-		log.Println("Failed to list buckets", err)
-		return
-	}
-
-	log.Println("Buckets:")
-	for _, bucket := range result.Buckets {
-		log.Printf("%s : %s\n", aws.StringValue(bucket.Name), bucket.CreationDate)
-	}
-
-	// Create Bucket
-	// Do not want
-	/*
-		result, err := svc.CreateBucket(&s3.CreateBucketInput{
-			Bucket: &bucket,
-		})
-		if err != nil {
-			log.Println("Failed to create bucket", err)
-			return
-		}
-
-		if err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{Bucket: &bucket}); err != nil {
-			log.Printf("Failed to wait for bucket to exist %s, %s\n", bucket, err)
-			return
-		}
-
-	*/
-
-	// TODO: define Bucket name
-	// Replace Body with file IO reader.
-	// TODO define keyname
-	bucket := "kittyspy"
-	key := "test"
-	uploadResult, err := svc.PutObject(&s3.PutObjectInput{
-		Body:   strings.NewReader("Hello World!"),
-		Bucket: &bucket,
-		Key:    &key,
-	})
-	if err != nil {
-		log.Printf("Failed to upload data to %s/%s, %s\n", bucket, key, err.Error())
-		return
-	}
-
-	log.Printf("Upload Result", uploadResult)
-	//log.Printf("Successfully created bucket %s and uploaded data with key %s\n", bucket, key)
-
-	//log.Printf("Uploaded to ", uploadResult.Location(), "ver:", uploadResult.VersionID, "id:", uploadResult.UploadID)
-
 }
