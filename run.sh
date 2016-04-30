a7735435bad25d1437de080d1947793c37d90b7d
Mike Sergio <mikeserg@gmail.com>
2016-04-30 19:52:17 -0400

    Can upload content.

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 ID=AKIAI3HFPOIYCB4J6L3Q
 KEY=96vA1FsK2rEFq8xkuwy0++K7Op4lJwJL0CQ+clV3
 
-AWS_KEY_ID=$ID AWS_ACCESS_KEY=$KEY go run cmd/kittyspyupload/main.go
+# export AWS_ACCESS_KEY_ID=$ID 
+# export AWS_SECRET_ACCESS_KEY=$KEY 
+AWS_ACCESS_KEY_ID=$ID AWS_SECRET_ACCESS_KEY=$KEY go run cmd/kittyspyupload/main.go
