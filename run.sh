61b2b9fedf0e7c29dea6f83c9bef1f92586a2d47
Mike Sergio <mikeserg@gmail.com>
2016-05-04 07:36:13 -0400

    Run only KS camera.

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ KEY=96vA1FsK2rEFq8xkuwy0++K7Op4lJwJL0CQ+clV3
 
 # export AWS_ACCESS_KEY_ID=$ID 
 # export AWS_SECRET_ACCESS_KEY=$KEY 
-AWS_ACCESS_KEY_ID=$ID AWS_SECRET_ACCESS_KEY=$KEY go run cmd/kittysnapupload/main.go
+AWS_ACCESS_KEY_ID=$ID AWS_SECRET_ACCESS_KEY=$KEY go run cmd/kittysnapupload/kittysnap.go
