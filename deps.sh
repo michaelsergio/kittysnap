e2e6a313bf98af4cafa997893175bda51eb35436
Michael Sergio <mikeserg@gmail.com>
2016-05-08 14:42:53 -0400

    Merged and fixed cron job.

---
+++ deps.sh
@@ -0,0 +1,2 @@
+go get "github.com/aws/aws-sdk-go/aws" 
+go get "github.com/lazywei/go-opencv/opencv" 
--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 # AWS Credentials for kittysnap.
-ID=AKIAI3HFPOIYCB4J6L3Q
-KEY=96vA1FsK2rEFq8xkuwy0++K7Op4lJwJL0CQ+clV3
-
-# export AWS_ACCESS_KEY_ID=$ID 
-# export AWS_SECRET_ACCESS_KEY=$KEY 
+export AWS_ACCESS_KEY_ID=$ID 
+export AWS_SECRET_ACCESS_KEY=$KEY 
 AWS_ACCESS_KEY_ID=$ID AWS_SECRET_ACCESS_KEY=$KEY go run cmd/kittysnapupload/kittysnap.go
